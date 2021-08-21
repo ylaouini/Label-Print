9ae2d9194e1510e716b5e30014ff98ba6fbd8064
Yassine LAOUINI <laouini.info@gmail.com>
2021-08-21 21:10:24 +0100

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Label_Print
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnCSVSelect = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(1145, 503);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(75, 23);
+            this.btnPrint.TabIndex = 0;
+            this.btnPrint.Text = "Print";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(92, 575);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CSV";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(153, 572);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(762, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 122);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1239, 301);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // btnCSVSelect
+            // 
+            this.btnCSVSelect.Location = new System.Drawing.Point(918, 570);
+            this.btnCSVSelect.Name = "btnCSVSelect";
+            this.btnCSVSelect.Size = new System.Drawing.Size(41, 23);
+            this.btnCSVSelect.TabIndex = 4;
+            this.btnCSVSelect.Text = "...";
+            this.btnCSVSelect.UseVisualStyleBackColor = true;
+            this.btnCSVSelect.Click += new System.EventHandler(this.btnCSVSelect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1263, 621);
+            this.Controls.Add(this.btnCSVSelect);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnPrint);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnCSVSelect;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Label_Print
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static string path = Application.StartupPath;
+        public static String[,] array = new String[2000, 5];
+        public static int ligne = 0;
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            String printer = "";
+            for (int line = 0; line < ligne; line++)
+            {
+                string str = File.ReadAllText(Application.StartupPath + "/label.prn");
+                str = str.Replace("@xcode", array[line, 0]);
+                str = str.Replace("@epn", array[line, 1]);
+                str = str.Replace("@cordX", array[line, 2]);
+                str = str.Replace("@cordY", array[line, 3]);
+                System.Threading.Thread.Sleep(1500);
+                //File.WriteAllText(Application.StartupPath + "/tmp.prn", str);
+                //System.Diagnostics.Process.Start("cmd.exe", "/c print " + Application.StartupPath + "\\tmp.prn");
+                if (line == 0)
+                {
+                    PrintDialog pd = new PrintDialog();
+                    pd.PrinterSettings = new PrinterSettings();
+                    if (DialogResult.OK == pd.ShowDialog(this))
+                    {
+                      
+                        RawPrinterHelper.SendStringToPrinter(pd.PrinterSettings.PrinterName, str);
+                        printer = pd.PrinterSettings.PrinterName;
+                    }
+
+                }
+                else
+                {
+                    RawPrinterHelper.SendStringToPrinter(printer, str);
+                }
+                File.Delete(Application.StartupPath + "/tmp.prn");
+
+            }
+        }
+
+        private void btnCSVSelect_Click(object sender, EventArgs e)
+        {
+            string fileName = null;
+            using (OpenFileDialog openFileDialog1 = new OpenFileDialog())
+            {
+                openFileDialog1.InitialDirectory = path;
+                openFileDialog1.Filter = "csv files (*.csv)|*.csv";
+                openFileDialog1.FilterIndex = 2;
+                openFileDialog1.RestoreDirectory = true;
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    fileName = openFileDialog1.FileName;
+                }
+            }
+
+            if (fileName != null)
+            {
+                //Do something with the file, for example read text from it
+                path = fileName;
+                textBox1.Text = path;
+            }
+            using (var reader = new StreamReader(path))
+            {
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(';');
+                    array[ligne, 0] = values[0];
+                    array[ligne, 1] = values[1];
+                    array[ligne, 2] = values[2];
+                    array[ligne, 3] = values[3];
+                    ligne++;
+                }
+            }
+        }
+    }
